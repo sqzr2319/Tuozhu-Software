405a55fd22474594e39fa8bc30f38a938f3233ce
knoxthecrier <17701655569@163.com>
2025-02-16 03:56:24 +0800

    添加Unity2D项目

---
+++ Tuozhu/Assets/Scripts/GameData.cs
@@ -0,0 +1,9 @@
+[System.Serializable]
+public class GameData
+{
+    public bool isFirstTimePlaying = true;//是否初次进入游戏
+    public int currentLevel = 0;//当前关卡进度
+    public float musicVolume = 1.0f;//背景音乐音量
+    public float soundVolume = 1.0f;//音效音量
+    public bool isGameFinished = false;//是否完成游戏
+}
